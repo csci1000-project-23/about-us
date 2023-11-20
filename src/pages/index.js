ce6d6b84231b691f93108936ae0801634f517a6b
gwke3441 <143977316+gwke3441@users.noreply.github.com>
2023-11-19 23:01:00 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,6 +34,10 @@ export default function Home() {
             <h3>About Me Page &rarr;</h3>
             <p>Click here to navigate to the Carolyn Dockhorn about me page</p>
           </a>
+          <a href='/GwynKeaty ' className='card'>
+            <h3>Example About Me Page &rarr;</h3>
+            <p>Click here to navigate example page</p>
+          </a>
 
           <a
             href='https://github.com/vedant29/Project-CSCI-1000'
